c00a59d7d9893f2d3eca95c92017bf08d2c45821
Katsu-Shunsuke <64359144+Katsu-Shunsuke@users.noreply.github.com>
2023-04-11 15:13:13 +0900

    add scroll event

--- blog/static/javascript/script.js
+++ blog/static/javascript/script.js
@@ -1,17 +1,21 @@
-$('a[href^="#"]').click(function() {
-    var target = $($(this).attr('href'));
-    if (target.length) {
-        var top = target.offset().top;
-        $('html, body').animate({scrollTop: top}, 1000);
-        return false;
-    }
+$(document).ready(function() {
+    console.log("テスト");
+    $('a[href^="#"]').click(function() {
+        var target = $($(this).attr('href'));
+        console.log(target);
+        if (target.length) {
+            var top = target.offset().top;
+            $('html, body').animate({scrollTop: top}, 1000);
+            return false;
+        }
+    });
 });
 
 window.onload = function() {
     var changeColor = function() {
         var e = document.getElementById('test');
         e.style.color = 'red';
-        console.log("書き換えテスト")
+        console.log("書き換えテス")
     }
     setTimeout(changeColor, 5000);
 }
